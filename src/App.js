a059710ffc647922e9b42241d4356dac47c1c99c
Mateen Kazia <mateencode@gmail.com>
2019-08-19 20:01:15 -0700

    react router added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React, { PureComponent } from "react";
+import {
+  StreamCreate,
+  StreamDelete,
+  StreamEdit,
+  StreamList,
+  StreamShow
+} from "containers";
+import { Route } from "react-router-dom";
 
 class App extends PureComponent {
   render() {
-    return <>hello</>;
+    return (
+      <>
+        <Route path='/' exact component={StreamList} />
+        <Route path='/stream/new' exact component={StreamCreate} />
+        <Route path='/stream/edit' exact component={StreamEdit} />
+        <Route path='/stream/delete' exact component={StreamDelete} />
+        <Route path='/stream/show' exact component={StreamShow} />
+      </>
+    );
   }
 }
 
---
+++ src/containers/index.js
@@ -0,0 +1,7 @@
+import StreamCreate from "./streams/StreamCreate";
+import StreamDelete from "./streams/StreamDelete";
+import StreamEdit from "./streams/StreamEdit";
+import StreamList from "./streams/StreamList";
+import StreamShow from "./streams/StreamShow";
+
+export { StreamCreate, StreamDelete, StreamEdit, StreamList, StreamShow };
