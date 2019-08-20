9ec33752771ab1ea54c3515f0d627a4f693af22a
Mateen Kazia <mateencode@gmail.com>
2019-08-19 22:35:37 -0700

    google log in & log out toggling

--- src/components/sections/Header/GoogleAuth.js
+++ src/components/sections/Header/GoogleAuth.js
@@ -27,19 +27,27 @@ export class GoogleAuth extends PureComponent {
     });
   };
 
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
       return null;
     } else if (this.state.isSignedIn) {
       return (
-        <button className='ui red google button'>
+        <button onClick={this.onSignOut} className='ui red google button'>
           <i className='google icon' />
           Sign out
         </button>
       );
     } else {
       return (
-        <button className='ui red google button'>
+        <button onClick={this.onSignIn} className='ui red google button'>
           <i className='google icon' />
           Sign In with google
         </button>
