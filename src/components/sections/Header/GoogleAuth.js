e52b9a168f7ac75bf65a76107e2fa94b787e0520
Mateen Kazia <mateencode@gmail.com>
2019-08-19 22:31:54 -0700

    conditional rendoring oauth button

--- src/components/sections/Header/GoogleAuth.js
+++ src/components/sections/Header/GoogleAuth.js
@@ -29,11 +29,21 @@ export class GoogleAuth extends PureComponent {
 
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
-      return <div>I dont know if we are signed in</div>;
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div>I am signed in!</div>;
+      return (
+        <button className='ui red google button'>
+          <i className='google icon' />
+          Sign out
+        </button>
+      );
     } else {
-      return <div>I am not signed in</div>;
+      return (
+        <button className='ui red google button'>
+          <i className='google icon' />
+          Sign In with google
+        </button>
+      );
     }
   }
 
