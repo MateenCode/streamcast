f7869cfa1555a2f9adcdb432e3be40510af4482f
Mateen Kazia <mateencode@gmail.com>
2019-08-27 13:09:46 -0700

    created  Bulk Action Creators

--- client/src/configs/types.js
+++ client/src/configs/types.js
@@ -1,2 +1,7 @@
 export const SIGN_IN = "SIGN_IN";
 export const SIGN_OUT = "SING_OUT";
+export const CREATE_STREAM = "CREATE_STREAM";
+export const FETCH_STREAMS = "FETCH_STREAMS"
+export const FETCH_STREAMS = "FETCH_STREAM"
+export const DELETE_STREAMS = "DELETE_STREAM"
+export const EDIT_STREAM = "FETCH_STREAM"
