00565b5ab35ac20e6be1b5b856495ee7a99be0bf
Mateen Kazia <mateencode@gmail.com>
2019-08-27 16:12:42 -0700

    rendering all streams

--- client/src/containers/streams/StreamList.js
+++ client/src/containers/streams/StreamList.js
@@ -8,12 +8,35 @@ export class StreamList extends PureComponent {
     this.props.fetchStreams();
   }
 
+  renderList() {
+    return this.props.streams.map(stream => {
+      return (
+        <div className='item' key={stream.id}>
+          <i className='large middle aligned icon camera' />
+          <div className='content'>
+            {stream.title}
+            <div className='description'>{stream.description}</div>
+          </div>
+        </div>
+      );
+    });
+  }
+
   render() {
-    return <div>steams</div>;
+    return (
+      <>
+        <h2>Streams</h2>
+        <div className='ui celled list'>{this.renderList()}</div>
+      </>
+    );
   }
 }
 
+const mapStateToProps = state => {
+  return { streams: Object.values(state.streams) };
+};
+
 export default connect(
-  null,
+  mapStateToProps,
   { fetchStreams }
 )(StreamList);
