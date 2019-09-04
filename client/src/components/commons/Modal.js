deedd7eb21a797fc3f4128d4ea7ad8d66445cedd
Mateen Kazia <mateencode@gmail.com>
2019-09-04 14:04:21 -0700

    made modal resuable component

--- client/src/components/commons/Modal.js
+++ client/src/components/commons/Modal.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { history } from "../../helpers";
 
-const Modal = props => {
+const Modal = ({ title, content, actions }) => {
   return ReactDOM.createPortal(
     <div
       onClick={() => history.push("/")}
@@ -12,14 +12,9 @@ const Modal = props => {
         onClick={e => e.stopPropagation()}
         className='ui standard modal visible active'
       >
-        <div className='header'>Delete Stream</div>
-        <div className='content'>
-          Are you sure you want to delete this stream?
-        </div>
-        <div className='actions'>
-          <button className='ui primary button'>Delete</button>
-          <button className='ui button'>Cancel</button>
-        </div>
+        <div className='header'>{title}</div>
+        <div className='content'>{content}</div>
+        <div className='actions'>{actions}</div>
       </div>
     </div>,
     document.querySelector("#modal")
--- client/src/containers/streams/StreamDelete.js
+++ client/src/containers/streams/StreamDelete.js
@@ -2,11 +2,22 @@ import React from "react";
 import { Modal } from "../../components/commons";
 
 function StreamDelete() {
+  const actions = (
+    <>
+      <button className='ui button negative'>Delete</button>
+      <button className='ui button'>Cancel</button>
+    </>
+  );
+
   return (
-    <div>
+    <>
       StreamDelete
-      <Modal />
-    </div>
+      <Modal
+        title='Delete Stream'
+        content='Are you sure you want to delete this stream?'
+        actions={actions}
+      />
+    </>
   );
 }
 
